8e07385377da9850f94e66ea607502a73146122e
Mukoni <Rmukoni@gmail.com>
2023-09-09 15:06:47 +0200

    Add project files.

---
+++ Controllers/PersonController.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Assessment.Models;
+
+namespace Assessment.Controllers
+{
+    public class PersonController : ApiController
+    {
+        private DBModel db = new DBModel();
+
+        // GET: api/Person
+        public IQueryable<Person> GetPersons()
+        {
+            return db.Persons;
+        }
+
+        // GET: api/Person/5
+        [ResponseType(typeof(Person))]
+        public IHttpActionResult GetPerson(int id)
+        {
+            Person person = db.Persons.Find(id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(person);
+        }
+
+        // PUT: api/Person/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutPerson(int id, Person person)
+        {
+        
+
+            if (id != person.code)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(person).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PersonExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Person
+        [ResponseType(typeof(Person))]
+        public IHttpActionResult PostPerson(Person person)
+        {
+          
+
+            db.Persons.Add(person);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = person.code }, person);
+        }
+
+        // DELETE: api/Person/5
+        [ResponseType(typeof(Person))]
+        public IHttpActionResult DeletePerson(int id)
+        {
+            Person person = db.Persons.Find(id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            db.Persons.Remove(person);
+            db.SaveChanges();
+
+            return Ok(person);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool PersonExists(int id)
+        {
+            return db.Persons.Count(e => e.code == id) > 0;
+        }
+    }
+}
